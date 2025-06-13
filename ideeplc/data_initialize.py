095074e709ff9d5edec5d140640a31d28dd0b889
Alireza Nameni <64960208+Alirezak2n@users.noreply.github.com>
2025-06-13 17:27:12 +0200

    Update data_initialize.py
    Remove train, valid, and test data and just add prediction data

--- ideeplc/data_initialize.py
+++ ideeplc/data_initialize.py
@@ -1,8 +1,9 @@
 import platform
 from typing import Tuple, Optional, Union
-
+import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
+from iDeepLC.ideeplc.utilities import df_to_matrix
 
 
 # Making the pytorch dataset
@@ -19,49 +20,37 @@ class MyDataset(Dataset):
 
 
 def data_initialize(
-        dataset_name: Optional[str] = None,
+        csv_path: str,
         batch_size: int = 96,
         **kwargs
 ) -> Union[Tuple[DataLoader, DataLoader, DataLoader, np.ndarray],
 Tuple[DataLoader, DataLoader, DataLoader, DataLoader, np.ndarray]]:
     """
-    Initialize data loaders for training, and validation.
+    Initialize data loaders for prediction based on a CSV file containing raw peptide sequences.
 
-    :param eval_type: The type of evaluation ('20datasets', 'ptm', 'aa_glycine').
-    :param dataset_name: The specific dataset name to use, only required for aa_glycine.
-    :param test_aa: The test amino acid, only required for aa_glycine.
-    :param batch_size: The batch size for the DataLoader.
-    :param kwargs: Additional arguments that might be needed for specific datasets.
-    :return: The DataLoader objects for training, and validation, and the shape of the training data.
+    :param csv_path: Path to the CSV file containing raw peptide sequences.
+    :param batch_size: Batch size for DataLoader.
+    :return: DataLoader for prediction.
     """
+    # Load peptides from CSV file
+    df = pd.read_csv(csv_path)
 
-    dataset_path = f"../data/matrices/{dataset_name}/"
-    train_x = np.load(dataset_path + 'train_x.npy')
-    train_y = np.load(dataset_path + 'train_y.npy')
-    val_x = np.load(dataset_path + 'val_x.npy')
-    val_y = np.load(dataset_path + 'val_y.npy')
-    test_x = np.load(dataset_path + 'test_x.npy')
-    test_y = np.load(dataset_path + 'test_y.npy')
+    raw_peptides = df['peptide'].tolist()
 
-    # Create the PyTorch Dataset objects
-    train_dataset = MyDataset(train_x, train_y)
-    val_dataset = MyDataset(val_x, val_y)
-    test_dataset = MyDataset(test_x, test_y)
+    sequences, tr, predictions, errors = df_to_matrix(raw_peptides)
+
+    prediction_dataset = MyDataset(sequences, tr)
 
     # Set the number of workers based on the platform
     workers = 0 if platform.system() == 'Windows' else 4
 
-    # TODO: check if batch_size works for val and test
     # Create DataLoader objects
-    dataloader_train = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, pin_memory=True,
-                                  num_workers=workers)
-    dataloader_val = DataLoader(val_dataset, shuffle=False, batch_size=batch_size, pin_memory=True, num_workers=workers)
-    dataloader_test = DataLoader(test_dataset, shuffle=False, batch_size=batch_size, pin_memory=True,
+    dataloader_pred = DataLoader(prediction_dataset, shuffle=False, batch_size=batch_size, pin_memory=True,
                                  num_workers=workers)
 
     # passing the training X shape
-    for batch in dataloader_train:
+    for batch in dataloader_pred:
         x_shape = batch[0].shape
         break
 
-    return dataloader_train, dataloader_val, dataloader_test, x_shape
+    return dataloader_pred, x_shape
