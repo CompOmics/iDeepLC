6b60742d131d1a07b95817c784b7314c5a594196
Alireza Nameni <64960208+Alirezak2n@users.noreply.github.com>
2025-07-03 15:54:28 +0200

    Update figure.py
    Adapt figure script to new calibration

--- ideeplc/figure.py
+++ ideeplc/figure.py
@@ -1,64 +1,46 @@
 from pathlib import Path
-
+import os
 import matplotlib.pyplot as plt
 import numpy as np
-import torch
 from sklearn.metrics import mean_absolute_error
-from torch.utils.data import DataLoader
+from datetime import datetime
 
-from predict import evaluate_model
+def make_figures(predictions: list, ground_truth: list, input_file: str, calibrated: bool = False ,save_results: bool = True,
 
-
-def make_figures(
-        model: torch.nn.Module,
-        dataloader_test: DataLoader,
-        loss_fn: torch.nn.Module,
-        model_path: str,
-        save_results: bool = False,
-        eval_results: tuple = None
-):
+                 ):
     """
-    Generate figures based on evaluation type.
+    Create and save scatter plot of predicted vs observed retention times.
 
-    :param model: Trained PyTorch model.
-    :param dataloader_test: DataLoader for the test set.
-    :param loss_fn: Loss function for evaluation.
-    :param model_path: Path to the trained model.
-    :param save_results: Whether to save results as a CSV file.
-    :param eval_results: Evaluation results from `evaluate_model`.
+    Args:
+        predictions (list): List of predicted retention times.
+        ground_truth (list): List of observed retention times.
+        input_file (str): Path to the input file used for predictions.
+        calibrated (bool): Whether the predictions are calibrated. Default is False.
+        save_results (bool): Whether to save the figure. Default is True.
+
     """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Set correct device
 
-    # If eval_results is provided, use it instead of re-running evaluation
-    if eval_results:
-        loss_test, corr_test, output_test, y_test = eval_results
-    else:
-        # If eval_results is not provided, fall back to running evaluate_model (not recommended)
-        loss_test, corr_test, output_test, y_test = evaluate_model(model, dataloader_test,
-                                                                   loss_fn, torch.device("cpu"), model_path,
-                                                                   save_results)
-
-    # generate figures
-    plot_20datasets(y_test, output_test, model_path)
-
-
-def plot_20datasets(y_test, output_test, model_path):
-    """Generate scatter plot for 20datasets."""
-    mae_test = mean_absolute_error(y_test, output_test)
-    max_value = max(output_test)
+    mae_test = mean_absolute_error(ground_truth, predictions)
+    max_value = max(predictions)
 
     fig, ax = plt.subplots(figsize=(7, 7))
-    ax.scatter(y_test, output_test, c="b",
-               label=f"MAE: {mae_test:.3f}, R: {np.corrcoef(y_test, output_test)[0, 1]:.3f}", s=3)
+    ax.scatter(ground_truth, predictions, c="b",
+               label=f"MAE: {mae_test:.3f}, R: {np.corrcoef(ground_truth, predictions)[0, 1]:.3f}", s=3)
     plt.legend(loc="upper left")
     plt.xlabel("Observed Retention Time")
     plt.ylabel("Predicted Retention Time")
-    dataset_name = Path(model_path).parent.name
-    plt.title(f"Dataset: {dataset_name}")  # Add dataset name as title
+    timestamp = datetime.datetime.now().strftime("%Y%m%d")
+    input_file_name = os.path.splitext(os.path.basename(input_file))[0]
+    output_path = Path("data/output") / f"{input_file_name}_predictions_{timestamp}"
+    if calibrated:
+        plt.title(f"scatterplot (calibrated)")
+        output_path = output_path + "_calibrated.png"
+    else:
+        plt.title(f"scatterplot (not calibrated)")
+        output_path = output_path + ".png"
     plt.axis("scaled")
     ax.plot([0, max_value], [0, max_value], ls="--", c=".5")
     plt.xlim(0, max_value)
     plt.ylim(0, max_value)
-    save_path = model_path.replace("best.pth", "scatter_plot.png")
-    plt.savefig(save_path, dpi=300)
+    plt.savefig(output_path, dpi=300)
     plt.show()
