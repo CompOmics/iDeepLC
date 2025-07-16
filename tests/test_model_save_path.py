a26c94e85d399d19c623be85ef22cf3054903349
Alireza Nameni <64960208+Alirezak2n@users.noreply.github.com>
2025-07-16 16:56:53 +0200

    Model path is not string any more

--- tests/test_model_save_path.py
+++ tests/test_model_save_path.py
@@ -8,7 +8,6 @@ def test_get_model_save_path():
     model_path, model_dir, pretrained_path = get_model_save_path()
     assert isinstance(model_path, Path), "Model path should be a Path object"
     assert isinstance(model_dir, Path), "Model directory should be a Path object"
-    assert isinstance(pretrained_path, str), "Pretrained path should be a string"
     assert model_path.name == "pretrained_model.pth", "Model name should be 'pretrained_model.pth'"
 
 
