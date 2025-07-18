2b66d5141f548ac175ff5559ecd3d69127f12041
Alireza Nameni <64960208+Alirezak2n@users.noreply.github.com>
2025-07-18 13:46:18 +0200

    Update test_data_initialize.py
    Change test unit to check if modifications are also take into account.

--- tests/test_data_initialize.py
+++ tests/test_data_initialize.py
@@ -7,13 +7,13 @@ def test_data_initialize():
     test_csv_path = "ideeplc/example_input/Hela_deeprt.csv"  # Path to a sample test CSV file
     matrix_input, x_shape = data_initialize(csv_path=test_csv_path)
 
-    df_0 = pd.DataFrame(matrix_input[0][0])
+    df_0 = pd.DataFrame(matrix_input[0][3])
     expected = pd.DataFrame({
-    1: [-0.796144, -0.941486, 10.000000, 12.000000, 4.000000, 4.0],
-    2: [-0.145342, 0.958311, 11.000000, 12.000000, 2.000000, 2.0],
-    3: [1.546797, 1.007078, 11.000000, 18.000000, 4.000000, 2.0],
-    4: [-0.588486, -1.384630, 6.000000, 8.000000, 4.000000, 2.0]
-}, index=[0, 1, 2, 3, 4, 5])
+        1: [1.286264, 0.209934, 12.0, 23.0, 2.0, 5.0],
+        2: [-1.076330, -2.715786, 6.0, 11.0, 7.0, 2.0],
+        3: [-1.435417, 1.267611, 14.0, 16.0, 4.0, 2.0],
+        4: [-1.280370, -2.870834, 6.0, 10.0, 4.0, 2.0]
+    }, index=[0, 1, 2, 3, 4, 5])
 
     pd.testing.assert_frame_equal(df_0.iloc[:6, 1:5], expected, check_dtype=False)
     assert matrix_input is not None, "Matrix input should not be None"
