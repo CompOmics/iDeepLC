b4e694823b7b6601ed0269e26aeb5ad3b0603139
Alireza Nameni <64960208+Alirezak2n@users.noreply.github.com>
2025-09-23 14:24:04 +0200

    Update __init__.py

--- ideeplc/__init__.py
+++ ideeplc/__init__.py
@@ -1,7 +1,4 @@
 """iDeepLC: A deep Learning-based retention time predictor for unseen modified peptides with a novel encoding system"""
 
-<<<<<<< Updated upstream
-__version__ = "1.3.0"
-=======
 __version__ = "1.3.1"
->>>>>>> Stashed changes
+
